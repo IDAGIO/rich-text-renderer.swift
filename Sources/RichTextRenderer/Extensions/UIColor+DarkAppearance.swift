3262102c8a75bf9dc03b3deb2147d8dc85d45e50
Michael Thomas <mickythomas@gmx.de>
2021-01-14 10:14:40 +0100

    Add global color override

--- Sources/RichTextRenderer/Extensions/UIColor+DarkAppearance.swift
+++ Sources/RichTextRenderer/Extensions/UIColor+DarkAppearance.swift
@@ -2,9 +2,17 @@
 
 import UIKit
 
+// Set these values to override default system colors
+public struct Color {
+    public static var richTextColor: UIColor? = nil
+    public static var richTextBackgroundColor: UIColor? = nil
+}
+
 public extension UIColor {
     /// Returns `UIColor.systemBackground` color or fallbacks to white.
     static var rtrSystemBackground: UIColor {
+        if let richTextBackgroundColor = Color.richTextBackgroundColor { return richTextBackgroundColor }
+
         #if os(tvOS)
             return .white
         #else
@@ -18,6 +26,8 @@ public extension UIColor {
 
     /// Returns `UIColor.label` color or fallbacks to black.
     static var rtrLabel: UIColor {
+        if let richTextColor = Color.richTextColor { return richTextColor }
+
         if #available(iOS 13.0, *) {
             return .label
         } else {
